7a122e64d71a1349b00b8a5b627c9310e4bc5b37
Mahesh <Maheshk2104@gmail.com>
2023-07-10 08:40:03 +0530

    [Mahesh]Add: Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RegexDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex_Demo regex = new Regex_Demo();
+            regex.RegexEx();
+        }
+    }
+}
---
+++ Regex_Demo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    // Regex is a pattern that is used to check whether a given string matches matches that pattern
+    public class Regex_Demo
+    {
+        static string pattern = "^cen$";
+        // ^ indicates the start
+        // $ indicates the end
+
+        public void RegexEx()
+        {
+            Regex regex = new Regex(pattern);
+            if(regex.IsMatch("cen") )
+            {
+                Console.WriteLine("its a match");
+            }
+            else
+            {
+                Console.WriteLine("its different");
+            }
+        }
+    }
+}
